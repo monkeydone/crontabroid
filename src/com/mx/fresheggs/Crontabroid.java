96aba3d116ec8021ae0e3a430f7e92e391deba21
lijunjie <lijunjieone@gmail.com>
2013-04-10 23:00:10 +0800

    加入设置功能

--- src/com/mx/fresheggs/Crontabroid.java
+++ src/com/mx/fresheggs/Crontabroid.java
@@ -1,7 +1,4 @@
-
-
 package com.mx.fresheggs;
-
 
 import android.app.Activity;
 import android.app.AlarmManager;
@@ -12,109 +9,95 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
-
 public class Crontabroid extends Activity {
-    Toast mToast;
+	Toast mToast;
 
-    @Override
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.alarm_controller);
+		setContentView(R.layout.main);
 
-        // Watch for button clicks.
-        Button button =  (Button)findViewById(R.id.start_repeating);
-        button.setOnClickListener(mStartRepeatingListener);
-        button = (Button)findViewById(R.id.stop_repeating);
-        button.setOnClickListener(mStopRepeatingListener);
-    }
+		// Watch for button clicks.
+		Button button = (Button) findViewById(R.id.start_repeating);
+		button.setOnClickListener(mStartRepeatingListener);
+		button = (Button) findViewById(R.id.stop_repeating);
+		button.setOnClickListener(mStopRepeatingListener);
+		button=(Button)findViewById(R.id.restart_repeating);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EditText text=(EditText)findViewById(R.id.mytime);
+				Toast.makeText(getApplicationContext(), "time="+text.getText(),Toast.LENGTH_LONG).show();
+				cancelAlarm();
+				startAlarm(Integer.parseInt(text.getText().toString()));
+			}
+		});
+	}
 
-//    private OnClickListener mOneShotListener = new OnClickListener() {
-//        public void onClick(View v) {
-//            // When the alarm goes off, we want to broadcast an Intent to our
-//            // BroadcastReceiver.  Here we make an Intent with an explicit class
-//            // name to have our own receiver (which has been published in
-//            // AndroidManifest.xml) instantiated and called, and then create an
-//            // IntentSender to have the intent executed as a broadcast.
-//            Intent intent = new Intent(Crontabroid.this, OneShotAlarm.class);
-//            PendingIntent sender = PendingIntent.getBroadcast(AlarmController.this,
-//                    0, intent, 0);
-//
-//            // We want the alarm to go off 30 seconds from now.
-//            Calendar calendar = Calendar.getInstance();
-//            calendar.setTimeInMillis(System.currentTimeMillis());
-//            calendar.add(Calendar.SECOND, 30);
-//
-//            // Schedule the alarm!
-//            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-//            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
-//
-//            // Tell the user about what we did.
-//            if (mToast != null) {
-//                mToast.cancel();
-//            }
-//            mToast = Toast.makeText(AlarmController.this, R.string.one_shot_scheduled,
-//                    Toast.LENGTH_LONG);
-//            mToast.show();
-//        }
-//    };
 
-    private OnClickListener mStartRepeatingListener = new OnClickListener() {
-        public void onClick(View v) {
-            // When the alarm goes off, we want to broadcast an Intent to our
-            // BroadcastReceiver.  Here we make an Intent with an explicit class
-            // name to have our own receiver (which has been published in
-            // AndroidManifest.xml) instantiated and called, and then create an
-            // IntentSender to have the intent executed as a broadcast.
-            // Note that unlike above, this IntentSender is configured to
-            // allow itself to be sent multiple times.
-            Intent intent = new Intent(Crontabroid.this, RepeatingAlarm.class);
-            PendingIntent sender = PendingIntent.getBroadcast(Crontabroid.this,
-                    0, intent, 0);
-            
-            // We want the alarm to go off 30 seconds from now.
-            long firstTime = SystemClock.elapsedRealtime();
-            firstTime += 15*1000;
 
-            // Schedule the alarm!
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-            am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                            firstTime, 15*1000, sender);
+	private OnClickListener mStartRepeatingListener = new OnClickListener() {
+		public void onClick(View v) {
+			startAlarm(1);
+		}
 
-            // Tell the user about what we did.
-            if (mToast != null) {
-                mToast.cancel();
-            }
-//            mToast = Toast.makeText(Crontabroid.this, R.string.repeating_scheduled,
-//                    Toast.LENGTH_LONG);
-//            mToast.show();
-        }
-    };
+	};
 
-    private OnClickListener mStopRepeatingListener = new OnClickListener() {
-        public void onClick(View v) {
-            // Create the same intent, and thus a matching IntentSender, for
-            // the one that was scheduled.
-            Intent intent = new Intent(Crontabroid.this, RepeatingAlarm.class);
-            PendingIntent sender = PendingIntent.getBroadcast(Crontabroid.this,
-                    0, intent, 0);
-            
-            // And cancel the alarm.
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
-            am.cancel(sender);
+	private void startAlarm(int minutes) {
+		Intent intent = new Intent(Crontabroid.this, CrontabTip.class);
+		PendingIntent sender = PendingIntent.getBroadcast(Crontabroid.this, 0,
+				intent, 0);
 
-            // Tell the user about what we did.
-            if (mToast != null) {
-                mToast.cancel();
-            }
-//            mToast = Toast.makeText(Crontabroid.this, R.string.repeating_unscheduled,
-//                    Toast.LENGTH_LONG);
-//            mToast.show();
-        }
-    };
+		long firstTime = SystemClock.elapsedRealtime();
+//		firstTime += minutes* 60 * 1000;
+
+		// Schedule the alarm!
+		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime,
+				minutes * 60* 1000, sender);
+
+		// Tell the user about what we did.
+		if (mToast != null) {
+			mToast.cancel();
+		}
+		// mToast = Toast.makeText(Crontabroid.this,
+		// R.string.repeating_scheduled,
+		// Toast.LENGTH_LONG);
+		// mToast.show();
+	}
+
+	private void cancelAlarm() {
+		// Create the same intent, and thus a matching IntentSender, for
+		// the one that was scheduled.
+		Intent intent = new Intent(Crontabroid.this, CrontabTip.class);
+		PendingIntent sender = PendingIntent.getBroadcast(Crontabroid.this, 0,
+				intent, 0);
+
+		// And cancel the alarm.
+		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+		am.cancel(sender);
+
+		// Tell the user about what we did.
+		if (mToast != null) {
+			mToast.cancel();
+		}
+		// mToast = Toast.makeText(Crontabroid.this,
+		// R.string.repeating_unscheduled,
+		// Toast.LENGTH_LONG);
+		// mToast.show();
+	}
+
+	private OnClickListener mStopRepeatingListener = new OnClickListener() {
+		public void onClick(View v) {
+			cancelAlarm();
+		}
+
+	};
 }
-
