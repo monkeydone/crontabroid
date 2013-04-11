10cb37d03b02549d8633e03cb669b471fe4e5286
lijunjie <lijunjieone@gmail.com>
2013-04-11 20:53:03 +0800

    加入声音提示 去掉重启按钮，使用启动一个按钮来替换

--- src/com/mx/fresheggs/CrontabTip.java
+++ src/com/mx/fresheggs/CrontabTip.java
@@ -17,9 +17,15 @@
 package com.mx.fresheggs;
 
 
+import java.util.ArrayList;
+
+
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.AsyncTask;
 import android.os.Vibrator;
 import android.widget.Toast;
 
@@ -29,8 +35,60 @@ public class CrontabTip extends BroadcastReceiver
     public void onReceive(Context context, Intent intent)
     {
     	Vibrator b=(Vibrator) context.getSystemService(context.VIBRATOR_SERVICE);
-    	b.vibrate(3000);
+//    	b.vibrate(3000);
+    	b.vibrate(30);
+    	
+		BeepSound beepSound = BeepSound.getInstance(context, R.raw.sound_receive_push);
+		beepSound.play();
         Toast.makeText(context, "repeating_received", Toast.LENGTH_SHORT).show();
     }
+
+public static class BeepSound{
+	public static Context mContext;
+	public static int mResId;
+	
+	private BeepSound(Context context, int resId){
+		mContext = context;
+		mResId = resId;
+	}
+	
+	private static BeepSound instance = null;
+	public static BeepSound getInstance(Context context, int resId) {
+		if(instance==null) {
+			instance=new BeepSound(context,resId);
+		}
+		return instance;
+	}
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void play(){
+		int id = mResId;
+		ArrayList list = new ArrayList();
+		list.add(mContext);
+		list.add(id);
+		
+		 new AsyncTask<ArrayList, Integer, Void>(){
+			 @Override
+			protected Void doInBackground(ArrayList... params) {
+				 Context mContext = (Context) params[0].get(0);
+				 int resId = (Integer) params[0].get(1);
+				 SoundPool soundPool = new SoundPool(1, AudioManager.STREAM_RING, 1);
+				 int soundID = soundPool.load(mContext, resId, 100);
+				 AudioManager mgr = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+				 float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_RING);
+				 float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_RING);
+				 final float stream = streamVolumeCurrent / streamVolumeMax;
+				 SoundPool.OnLoadCompleteListener OnLoadCompleteListener = new SoundPool.OnLoadCompleteListener() {
+					 @Override
+					 public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+						 soundPool.play(sampleId, stream, stream, 1, 0, 1);
+					 }
+				 };
+				 soundPool.setOnLoadCompleteListener(OnLoadCompleteListener);
+				return null;
+			 }
+			 
+		 }.execute(list);
+	}
 }
 
+}
--- src/com/mx/fresheggs/Crontabroid.java
+++ src/com/mx/fresheggs/Crontabroid.java
@@ -16,6 +16,8 @@ import java.util.Calendar;
 
 public class Crontabroid extends Activity {
 	Toast mToast;
+	boolean mIsStart;
+	Button mStartButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +26,43 @@ public class Crontabroid extends Activity {
 		setContentView(R.layout.main);
 
 		// Watch for button clicks.
-		Button button = (Button) findViewById(R.id.start_repeating);
-		button.setOnClickListener(mStartRepeatingListener);
-		button = (Button) findViewById(R.id.stop_repeating);
+		mStartButton = (Button) findViewById(R.id.start_repeating);
+		mStartButton.setOnClickListener(mStartRepeatingListener);
+		Button button = (Button) findViewById(R.id.stop_repeating);
 		button.setOnClickListener(mStopRepeatingListener);
-		button=(Button)findViewById(R.id.restart_repeating);
-		button.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				EditText text=(EditText)findViewById(R.id.mytime);
-				Toast.makeText(getApplicationContext(), "time="+text.getText(),Toast.LENGTH_LONG).show();
-				cancelAlarm();
-				startAlarm(Integer.parseInt(text.getText().toString()));
-			}
-		});
+//		button=(Button)findViewById(R.id.restart_repeating);
+//		button.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				EditText text=(EditText)findViewById(R.id.mytime);
+//				Toast.makeText(getApplicationContext(), "time="+text.getText(),Toast.LENGTH_LONG).show();
+//				cancelAlarm();
+//				startAlarm(Integer.parseInt(text.getText().toString()));
+//			}
+//		});
 	}
 
 
 
 	private OnClickListener mStartRepeatingListener = new OnClickListener() {
 		public void onClick(View v) {
-			startAlarm(1);
+			EditText text = (EditText) findViewById(R.id.mytime);
+			int time = 1;
+			try {
+				time = Integer.parseInt(text.getText().toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (!mIsStart) {
+				startAlarm(time);
+				mIsStart = true;
+				mStartButton.setText("restart");
+			} else {
+				cancelAlarm();
+				startAlarm(time);
+
+			}
 		}
 
 	};
@@ -56,7 +73,7 @@ public class Crontabroid extends Activity {
 				intent, 0);
 
 		long firstTime = SystemClock.elapsedRealtime();
-//		firstTime += minutes* 60 * 1000;
+		firstTime += minutes* 60 * 1000;
 
 		// Schedule the alarm!
 		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
